71764701f87c3b8b4618ef3a7751e3884c56a817
Kinnan Kwok <kkwoker@hotmail.com>
2014-09-30 20:22:30 -0700

    admins

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,34 @@
 class Admin::UsersController < UsersController 
 
+	before_action :find_user, only: [:show, :edit]
+
+
 	def index
-		@users = User.all
+		@users = User.all.page(params[:page]).per(5)
 	end	
+
+	def show
+	end
+
+  def edit
+  end
+
+  def update
+		@user = User.find(params[:id])
+
+  	if @user.update_attributes(user_params)
+  		redirect_to admin_user_path(@user), notice: "Success"
+  	else
+  		render :edit
+  	end
+  end
+
+	def find_user 
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		  	params.require(:user).permit(:email, :firstname, :lastname)
+	end
+
 end
