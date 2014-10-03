de699aa78565f2c1e5149ba8814b40569541170a
Kinnan Kwok <kkwoker@hotmail.com>
2014-10-03 09:40:57 -0700

    routes minimized

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
 
 	RottenMangoes::Application.routes.draw do
-  get 'review/new'
-  get 'review/create'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'users/new'
-  get 'users/create'
 
 	resources :movies do
 		resources :reviews, only: [:new, :create]
-		
 	end
+
 	resources :users, only: [:new, :create]
 	resources :sessions, only: [:new, :create, :destroy]
 	root to: 'movies#index'
